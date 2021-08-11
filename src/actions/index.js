f4a922d06221dcc4ddddc82c09ab7097e2dfed42
Sowmya <sowmyashree97@gmail.com>
2021-08-11 20:26:52 +0530

    fix: secure connection

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { getProducts } from '../api/weather';
 import * as types from '../constants/ActionTypes';
 
-const URL = 'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
+const URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
 
 const successList = list => ({
   type: types.GET_WEATHER_INFO_SUCCESS,
